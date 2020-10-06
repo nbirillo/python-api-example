1260b62a9fd11227fd5d23481c21b9fd5837f646
Anastasiia.Birillo <nbirillo@mail.ru>
2020-10-06 21:00:30 +0300

    Add a small hint comment

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     api_handler = VkApiHandler()
     current_user = api_handler.get_current_user()
     df = pd.DataFrame(columns=list(current_user.__dict__.keys()))
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.append.html
     df = df.append(current_user.__dict__, ignore_index=True)
 
     users = [api_handler.get_user_by_id(id) for id in range(1, 10)]
